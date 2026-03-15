43da275da2207212de91f314c8f7447e01e9a8c7
echoeyecodes <femiobajuluwa@gmail.com>
2026-03-15 14:18:00 +0100

    fix lint

--- server/src/lib/env/index.ts
+++ server/src/lib/env/index.ts
@@ -1,6 +1,7 @@
 import dotenv, { type DotenvConfigOptions } from "dotenv";
 
-const envFile: DotenvConfigOptions | undefined = process.env.NODE_ENV === "test" ? { path: ".env.test" } : undefined
+const envFile: DotenvConfigOptions | undefined =
+    process.env.NODE_ENV === "test" ? { path: ".env.test" } : undefined;
 
 dotenv.config(envFile);
 
